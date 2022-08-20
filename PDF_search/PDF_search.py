4a3e02ae8115886c996d3644a578a443e2a165e9
FayeSGW <108031168+FayeSGW@users.noreply.github.com>
2022-08-20 21:02:01 +0200

    Added comments and error messages

--- PDF_search/PDF_search.py
+++ PDF_search/PDF_search.py
@@ -1,93 +1,132 @@
 from pdfminer.high_level import extract_text
 import tkinter as tk
 from tkinter import EXTENDED, ttk, filedialog
-import re, os, sys, docx, winshell
+import re, os, docx, winshell
 
 class Search(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.lst = []
-
         #GUI Base
         self.title("Keyword Search")
+        self.intro = ttk.Frame(self, padding = "10")
+        self.intro.grid(column = 0, row = 0)
         self.foldersearch = ttk.Frame(self, padding = "10")
-        self.foldersearch.grid(column = 0, row = 0)
+        self.foldersearch.grid(column = 0, row = 1)
         self.keywordsearch = ttk.Frame(self)
-        self.keywordsearch.grid(column = 0, row = 1)
+        self.keywordsearch.grid(column = 0, row = 2)
         self.submit = ttk.Frame(self)
-        self.submit.grid(column = 0, row = 2)
+        self.submit.grid(column = 0, row = 3)
 
-        #Folder Search
-        self.folderbox = ttk.Label(self.foldersearch, text = "", borderwidth = 3, relief= "solid")
-        self.folderbox.config(width = 50, background = "white")
-        self.folderbox.grid(column = 0, row = 0)
-        folderbtn = ttk.Button(self.foldersearch, text = "Select Folder", command = self.openfolder)
-        folderbtn.grid(column = 1, row = 0)
+        #Intro and explanation
+        introtext = ttk.Label(self.intro, text = "Use this programme to search for a keyword (or key phrase) in pdf and Word files.")
+        introtext.config(wraplength= 400)
+        introtext.grid(column = 0, row = 0)
 
-        #Keyword Search
+        #Folder Selection
+        fldrtext = ttk.Label(self.foldersearch, text = "Select folder to search in:")
+        fldrtext.grid(column = 0, row = 0)
+        self.folderbox = ttk.Label(self.foldersearch, text = "")
+        self.folderbox.config(width = 50, background = "white", borderwidth = 3, relief = "solid")
+        self.folderbox.grid(column = 0, row = 1)
+        folderbtn = ttk.Button(self.foldersearch, text = "Browse", command = self.openfolder)
+        folderbtn.grid(column = 1, row = 1)
+
+        #Keyword Input
+        kwordtext = ttk.Label(self.keywordsearch, text = "Type keyword or key phrase to search for:")
+        kwordtext.grid(column = 0, row = 0)
         self.kword = tk.StringVar()
         self.keyword_entry = ttk.Entry(self.keywordsearch, textvariable = self.kword)
-        self.keyword_entry.grid(column = 0, row = 0)
+        self.keyword_entry.grid(column = 0, row = 1)
 
         #Submit
         self.submitbtn = ttk.Button(self.submit, text = "Search", command = self.search)
         self.submitbtn.grid(column = 0, row = 0)
 
     def openfolder(self):
+        #Dialog box to choose folder to search
         self.open_folder = filedialog.askdirectory()
         self.folderbox.config(text = self.open_folder)
 
     def search(self):
-        self.lst = []
-        folder = os.listdir(self.open_folder)
-        for filename in folder:
-            file = self.open_folder + "\\" + filename
-            if filename.endswith(".pdf"):
-                f = extract_text(file)
-                self.keyword(filename, f)
-            elif filename.endswith(".doc") or file.endswith(".docx"):
-                doc = docx.Document(file)
-                texts = []
-                for para in doc.paragraphs:
-                    texts.append(para.text)
-                fulltext = " ".join(texts)
-                self.keyword(filename, fulltext)
-        if len(self.lst) == 0:
-            self.lst.append("No files found!")
-        self.newwindow()
+        #Prompt user to input a keyword if they haven't
+        if self.kword.get() == "":
+            self.error("keyword")
+        else:
+            try:
+                self.lst = []
+                folder = os.listdir(self.open_folder)
+                #Extract text from pdf and Word files to search for keyword
+                for filename in folder:
+                    file = self.open_folder + "\\" + filename
+                    if filename.endswith(".pdf"):
+                        f = extract_text(file)
+                        self.keyword(filename, f)
+                    elif filename.endswith(".doc") or file.endswith(".docx"):
+                        doc = docx.Document(file)
+                        texts = []
+                        for para in doc.paragraphs:
+                            texts.append(para.text)
+                        fulltext = " ".join(texts)
+                        self.keyword(filename, fulltext)
+                #If keyword not found in any files
+                if len(self.lst) == 0:
+                    self.lst.append("No files found!")
+                
+                self.newwindow()
+            except AttributeError:
+                #Prompts user to choose a folder is they haven't
+                self.error("folder")
+
+    def error(self, label):
+        #Message to prompt user to input folder or keyword if they haven't
+        wind = tk.Toplevel(self)
+        self.title("Error!")
+        self.frame = ttk.Frame(wind, padding = "10")
+        self.frame.grid(column = 0, row = 0)
+        aerrorlbl = ttk.Label(self.frame, text = f"Please choose a {label} to search!")
+        aerrorlbl.grid(column = 0, row = 0)
 
     def keyword(self, filename, txt):
+        #Search extracted text for selected keyword
         search = re.search(self.kword.get(), txt, re.IGNORECASE)
         if search:
             self.lst.append(filename)
 
 
     def newwindow(self):
+        #Open new window with list of files containing keyword
         wind = tk.Toplevel(self)
-        wind.geometry("300x200")
         self.title("Files Containing Keyword")
         self.boxframe = ttk.Frame(wind, padding = "10")
         self.boxframe.grid(column = 0, row = 0)
         self.buttonsframe = ttk.Frame(wind, padding = "10")
         self.buttonsframe.grid(column = 1, row = 0)
 
+        boxlbl = ttk.Label(self.boxframe, text = "Files containing keyword:")
+        boxlbl.grid(column = 0, row = 0)
         filelist = tk.StringVar(value = self.lst)
-        self.box = tk.Listbox(self.boxframe, listvariable = filelist, selectmode = EXTENDED)
-        self.box.grid(column = 0, row = 0)
+        self.box = tk.Listbox(self.boxframe, listvariable = filelist, selectmode = EXTENDED, width = 50)
+        self.box.grid(column = 0, row = 1)
+
+        buttonstext = ttk.Label(self.buttonsframe, text = "Select files you are interested in. \n These can be opened externally using the button below. \n You can also save shortcuts to the selected files in a new subfolder, for easy perusal at a later time.")
+        buttonstext.grid(column = 0, row = 0)
         openbtn = ttk.Button(self.buttonsframe, text = "Open Files", command = self.openfiles)
-        openbtn.grid(column = 0, row = 0)
+        openbtn.grid(column = 0, row = 1)
         savebtn = ttk.Button(self.buttonsframe, text = "Save", command = self.savefiles)
-        savebtn.grid(column = 0, row = 1)
+        savebtn.grid(column = 0, row = 2)
 
     def openfiles(self):
+        #Opens selected files
         files = self.box.curselection()
         for i in files:
             f = os.path.join(self.open_folder, self.box.get(i))
             os.startfile(f)
 
     def savefiles(self):
-        source = os.path.join(self.open_folder, self.kword.get())
+        #Saves shortcuts to selected files in new folder
+        foldername = "Keyword - " + f"'{self.kword.get()}'"
+        source = os.path.join(self.open_folder, foldername)
         try:
             os.mkdir(source)
         except FileExistsError:
--- PDF_search/check.py
+++
@@ -1,5 +0,0 @@
-from tkinter import Tk, filedialog
-
-root = Tk()
-
-open_file = filedialog.askdirectory()
